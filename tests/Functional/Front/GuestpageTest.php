74b02e43fb78b56d613b1ac5e11194ed84d7d6c3
Jing Zhang <zhangjing0823@gmail.com>
2026-02-01 18:41:13 +0100

    fix(guests-page-test): ensure blocked guests are not displayed

--- tests/Functional/Front/GuestpageTest.php
+++ tests/Functional/Front/GuestpageTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional\Front;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class GuestpageTest extends WebTestCase
@@ -11,7 +12,9 @@ final class GuestpageTest extends WebTestCase
     {
         // open guests list
         $client = static::createClient();
+
         $crawler = $client->request('GET', '/guests');
+        
         $this->assertResponseIsSuccessful();
 
         // assert the page title
@@ -37,5 +40,25 @@ final class GuestpageTest extends WebTestCase
         $this->assertResponseIsSuccessful();
 
         $this->assertMatchesRegularExpression('#^/guest/\d+$#', $client->getRequest()->getPathInfo());
+    }
+
+    public function testGuestPagesDoesNotShowBlockedGuests(){
+         // open guests list
+        $client = static::createClient();
+
+        /** @var UserRepository $usersRepo */
+        $usersRepo= static::getContainer()->get(UserRepository::class);
+        $blockedGuest = $usersRepo->findOneBy(['email'=> 'blockedGuest@test.com']);
+        $this->assertNotNull($blockedGuest, "L'invité bloqué n'est pas trouvé dans la base des données");
+
+        
+        $crawler = $client->request('GET', '/guests');
+        $this->assertResponseIsSuccessful();
+
+        $idOfBlockedGuest = $blockedGuest->getId();
+        $selector = sprintf('.guest .guest[data-guest-id="%d"]', $idOfBlockedGuest);
+        
+        $this->assertCount(0, $crawler->filter($selector));
+
     }
 }
